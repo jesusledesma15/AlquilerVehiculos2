2d5e62c761d192dcaec53307e76320ff3ba32eb4
jesusledesma15 <jesusproint998@gmail.com>
2019-03-03 12:52:22 +0100

    Metodo generarClienteAleatorio()

--- src/empresalquiler2/Cliente.java
+++ src/empresalquiler2/Cliente.java
@@ -28,7 +28,78 @@ public class Cliente {
         this.nombre = "nomCliente";
         this.apellidos = "apelCliente";
     }
-    
+
+    public static Cliente generarClienteAleatorio() {
+
+        //GENERAR CLIENTE ALEATORIO
+        //Generar nombre aleatorio:
+        Random aleatorio = new Random();
+        String nombre = "", apellidos = "", nif;
+        int numCliente = aleatorio.nextInt(4);
+        switch (numCliente) {
+            case 0:
+                nombre = "Jesús";
+                break;
+            case 1:
+                nombre = "Manuel";
+                break;
+            case 2:
+                nombre = "Juan";
+                break;
+            case 3:
+                nombre = "David";
+                break;
+            case 4:
+                nombre = "Cristian";
+                break;
+
+        }
+
+        //Generar apellido aleatorio:
+        int apellidoCliente = aleatorio.nextInt(4);
+        switch (apellidoCliente) {
+            case 0:
+                apellidos = "Ledesma";
+                break;
+            case 1:
+                apellidos = "Fargas";
+                break;
+            case 2:
+                apellidos = "Álvarez";
+                break;
+            case 3:
+                apellidos = "Reyes";
+                break;
+            case 4:
+                apellidos = "Carrasco";
+                break;
+
+        }
+        //Números del NIF:
+        int num1 = aleatorio.nextInt(10);
+        int num2 = aleatorio.nextInt(10);
+        int num3 = aleatorio.nextInt(10);
+        int num4 = aleatorio.nextInt(10);
+        int num5 = aleatorio.nextInt(10);
+        int num6 = aleatorio.nextInt(10);
+        int num7 = aleatorio.nextInt(10);
+        int num8 = aleatorio.nextInt(10);
+        String[] numNif = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        String numeroNif = numNif[num1] + numNif[num2] + numNif[num3] + numNif[num4] + numNif[num5] + numNif[num6] + numNif[num7] + numNif[num8];
+
+        //Calcular letra DNI:
+        int posicionLetra = aleatorio.nextInt(26);
+        String[] letra = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        String letraNif = letra[posicionLetra];
+
+        //Resultado:        
+        nif = numeroNif + letraNif;
+        Cliente c = new Cliente(nif, nombre, apellidos);
+        return c;
+    }
+
     public String getNif() {
         return nif;
     }
